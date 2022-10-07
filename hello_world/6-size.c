1dcb212be5cbbc6df43b21614ce298e1d1223229
Natha0b <laura.barona04@gmail.com>
2022-10-06 22:46:25 -0700

    C - Hello, World

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - for print
+* Description: command print text
+* Return: cero
+*/
+int main(void)
+{
+
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long-int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(double-int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
+
+}
