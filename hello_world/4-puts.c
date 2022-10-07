d9b34b073edc97c686a47651d49f6e7c58196ec8
Natha0b <laura.barona04@gmail.com>
2022-10-06 22:09:28 -0700

    C - Hello, World

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - for print
+* Description: command print text
+* Return: zero to confirm everything went well
+*/
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
