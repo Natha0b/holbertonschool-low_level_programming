3f9e3ecfc02171f0bc4f10c5856185dd642d2cd4
Natha0b <laura.barona04@gmail.com>
2022-10-10 16:03:08 -0700

    C - Variables

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
-   printf("%d is positive\n", n);
+        printf("%d is positive\n", n);
 
 else if (n == 0)
         printf("%d is zero\n", n);
