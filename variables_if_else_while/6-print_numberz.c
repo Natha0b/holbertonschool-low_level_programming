638d299656f25cafe53f6494459debdd5956e735
Natha0b <laura.barona04@gmail.com>
2022-10-10 21:45:58 -0700

    C - Variables

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+
+int numbers;
+
+for (numbers = '0' ; numbers <= '9' ; numbers++)
+
+{
+putchar(numbers);
+}
+
+
+putchar('\n');
+
+return (0);
+
+}
