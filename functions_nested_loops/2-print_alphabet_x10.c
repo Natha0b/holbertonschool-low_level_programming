192be63f90dd45da2bb5c48e525964749f5a54bb
Natha0b <laura.barona04@gmail.com>
2022-10-11 11:58:48 -0700

    Functions, nested loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - print
+ */
+void print_alphabet(void)
+{
 
+char alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+for (char alphabet = 0; alphabet < 10; alphabet++)
+
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+
+}
