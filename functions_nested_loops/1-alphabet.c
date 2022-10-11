4d45624befb92d3903733ab7f52e06edeecbd0db
Natha0b <laura.barona04@gmail.com>
2022-10-11 08:15:38 -0700

    Functions, nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Alphabet
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+
+char alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
