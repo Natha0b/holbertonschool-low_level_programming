23c34e08246bea23162b17aa7daa874e8b097425
Natha0b <laura.barona04@gmail.com>
2022-10-10 20:46:28 -0700

    C - Variables

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  * main - Alphabet
+ *   *
+ *    * Return: zero
+ *     */
+
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 'a' 'A' ; alphabet <= 'z' 'Z' ; alphabet++)
+
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
