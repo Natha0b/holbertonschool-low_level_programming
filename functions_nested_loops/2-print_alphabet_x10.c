b605c8c9ac9d54c7417ff9ab07aba45885adc4be
Natha0b <laura.barona04@gmail.com>
2022-10-11 12:29:14 -0700

    Functions, nested loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,15 @@
 void print_alphabet_x10(void);
 
 {
-	
 char alphabet;
 int number = 0;
 
+
 while (number <= 9)
+
 {
 for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+
 {
 _putchar(alphabet);
 }
