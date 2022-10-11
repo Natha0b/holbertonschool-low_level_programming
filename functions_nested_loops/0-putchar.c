da2cee4a5a85b0e25ed4d80ba717fac3a62f566c
Natha0b <laura.barona04@gmail.com>
2022-10-11 07:53:53 -0700

    Functions, nested loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Functions, nested loops
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+
+printf( “_putchar” )
+
+putchar('\n');
+
+return (0);
+
+{
