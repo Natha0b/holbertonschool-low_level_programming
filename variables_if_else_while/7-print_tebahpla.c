4e4ece778b228c3342a22b7725780d64d0406d28
Natha0b <laura.barona04@gmail.com>
2022-10-10 21:49:48 -0700

    C - Variables

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Alphabet
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+
+char alphabet;
+
+	
+for (alphabet = 'z' ; alphabet <= 'a' ; alphabet++)
+
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
