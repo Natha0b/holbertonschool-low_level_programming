3c88292ccd89f31b4ac9fb4c7502409037fb292d
Natha0b <laura.barona04@gmail.com>
2022-10-11 14:46:23 -0700

    Functions, nested loops

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_islower - check lowercase
+ *
+ *@c: character check
+ * 
+ * Return: zero or one
+ */
+
+int print_sign(int n);
+
+{
+if (n > 0);
+{
+printf('+')
+return (1);
+}
+if else (n == 0);
+printf('0')
+return (0)
+}
+{
+else
+printf('-')
+return (-1);
+}
