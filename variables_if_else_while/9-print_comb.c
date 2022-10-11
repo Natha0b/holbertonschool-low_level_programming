68986eb54837eb1d96d92f9e0adec41d89230523
Natha0b <laura.barona04@gmail.com>
2022-10-10 22:09:18 -0700

    C - Variables

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+
+int numbers;
+
+for (numbers = '0' ; numbers <= '9' ; numbers++)
+
+{
+putchar(numbers);
+}
+
+if (numbers != '9')
+{
+putchar(',');
+putchar(' ');
+putchar('\n');
+}
+
+return (0);
+
+}
