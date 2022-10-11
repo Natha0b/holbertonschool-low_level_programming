1c3435eca1486cb83767965ee95e284bfa24e388
Natha0b <laura.barona04@gmail.com>
2022-10-11 14:15:12 -0700

    Functions, nested loops

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check lowercase
+ * @c: character check
+ *
+ * Return: zero or one
+ */
+
+int _isalpha(int c)
+
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+else
+	
+return (0);
+}
