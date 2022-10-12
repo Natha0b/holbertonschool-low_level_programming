7962cce2527aef5f0d53353ab5d32576ce4cba67
Natha0b <laura.barona04@gmail.com>
2022-10-12 13:40:49 -0700

    Functions, nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  *jack_bauer - every minute of the day 
- *@h: character check
- *@m: character check
+ *@hora: character check
+ *@minuto: character check
  */
 
 void jack_bauer(void)
@@ -11,17 +11,18 @@ void jack_bauer(void)
 int hora;
 int minuto;
 
-for (hora = 0; hora < 23; hora++)
+for (hora = 0; hora <= 23; hora++)
 {
-for (minuto = 0; minuto < 59; minuto++)
+for (minuto = 0; minuto <= 59; minuto++)
 {
 _putchar((hora / 10) + '0');
 _putchar((hora % 10) + '0');
+_putchar('\n');
 _putchar(':');
 _putchar((minuto / 10) + '0');
 _putchar((minuto % 10) + '0');
-}
 _putchar('\n');
+}
 }
 }
 
