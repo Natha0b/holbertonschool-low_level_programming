8ee2ba7943b908447fdb3cff04940bd92cf0f25c
Natha0b <laura.barona04@gmail.com>
2022-10-12 12:26:31 -0700

    Functions, nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,3 +9,21 @@
 
 void jack_bauer(void)
 
+int hora;
+int minuto;
+{
+for (hora = 0; hora <= 23; hora++)
+for (minuto = 0; minuto <= 59; minuto++)
+
+_putchar(hora / 10 + '0');
+_putchar(hora % 10 + '0');
+_putcha(':')
+_putchar(minutos / 10 + '0');
+_putchar(minutos % 10 + '0');
+_putchar('\n')
+
+}	
+
+
+
+
