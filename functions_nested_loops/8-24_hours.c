e72297f24def5612f380616737f5f203d5f7d666
Natha0b <laura.barona04@gmail.com>
2022-10-12 13:23:17 -0700

    Functions, nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ for (hora = 0; hora <= 23; hora++)
 {
 for (minuto = 0; minuto <= 59; minuto++)
 
-_putchar(hora / 10 + '0');
-_putchar(hora % 10 + '0');
+_putchar((hora / 10) + '0');
+_putchar((hora % 10) + '0');
 _putchar(':');
-_putchar(minuto / 10 + '0');
-_putchar(minuto % 10 + '0');
+_putchar((minuto / 10) + '0');
+_putchar((minuto % 10) + '0');
 _putchar('\n');
 }
 }
