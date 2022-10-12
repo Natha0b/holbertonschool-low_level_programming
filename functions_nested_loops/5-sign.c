ffa56b0dc30ac4403a11473ed46f03561ad06558
Natha0b <laura.barona04@gmail.com>
2022-10-12 07:29:55 -0700

    Functions, nested loops

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,22 +10,22 @@ int print_sign(int n)
 
 {
 
-if (n > 0);
+if (n > 0)
 {
-_putchar('+')
+_putchar('+');
 return (1);
 }
 
 
-else if (n == 0);
+else if (n == 0)
 {
-_putchar('0')
+_putchar('0');
 return (0);
 }
 
 else
 {
-printf('-')
+printf('-');
 return (-1);
 }
 }
