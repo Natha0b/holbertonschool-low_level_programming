cd8dc56fbe7124bff4206f9834f7fdc88912d6c8
Natha0b <laura.barona04@gmail.com>
2022-10-12 14:22:53 -0700

    Functions, nested loops

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *times_table - prints the 9 times table
+ */
+
+void times_table(void)
+{
+int m;
+int n;
+int l;
+
+{
+for(m= 1; m = 9; m++)
+for(n= 1; j <= l; n++)
+_putchar(m * n = l)
+}
+}
