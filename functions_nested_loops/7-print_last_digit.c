a2a1e174456cf830b8da1f7ea06786a89386c0c5
Natha0b <laura.barona04@gmail.com>
2022-10-12 09:45:45 -0700

    Functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+/**
+ *_islower - check lowercase
+ *@n: character check
+ *
+ *Return: zero or one
+ */
+
+int print_last_digit(int n)
+{
+int l;
+l = n % 10;
+
+if (n < 0)
+
+{
+l = l * -1;
+putchar(l + '0');
+return (l);
+
+}
+
+else
+{	
+putchar(l + '0);
+return (l);
+}	
