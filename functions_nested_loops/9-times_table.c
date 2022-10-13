fb34cceccbef565e5f5375ef5c18665920e28921
Natha0b <laura.barona04@gmail.com>
2022-10-12 19:14:58 -0700

    Functions, nested loops

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,13 +7,33 @@ void times_table(void)
 {
 int filas;
 int colum;
-int ;
+int c;
 
 {
-for(m= 1; m = 9; m++)
-
-_putchar(%d * %d = %d);
-_putchar(',')
-_putchar(' ')
+for(filas = 0; filas <= 9; filas++)
+{
+for(colum = 0; colum <= 9; colum++)
+{
+int ope;
+ope = c * colum;
+if (c > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
+else if (colum == 0)
+{
+_putchar(c + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c + '0');
+}
+}
+c++;
+_putchar('\n);
 }
