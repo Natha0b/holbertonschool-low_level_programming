f569aef087fde0f86ced0988ed3d2af987637cde
Natha0b <laura.barona04@gmail.com>
2022-10-12 19:56:18 -0700

    Functions, nested loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * @a: check value
+ * @b: check value
+ * add - numbers
+ *Return: a and b
+ */
+
+int add(int a, int b)
+{
+
+int result;
+
+result = a + b;
+return (result);
+
+}
+
