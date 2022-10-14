c03cf0c05122a246636dd197efe8b8d222146570
Natha0b <laura.barona04@gmail.com>
2022-10-14 13:13:00 -0700

    task 3

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *print_numbers - print numbers
+ *numbers - variable
+ *_putchar: print caracters
+ */
+void print_most_numbers(void)
+{
+
+char numbers;
+
+for (numbers = '0' ; numbers <= '9' ; numbers++)
+
+if (numbers != '2' && numbers != '4')
+
+{
+putchar(numbers);
+}
+
+putchar('\n');
+
