c12498a35fcc148b98de0e5e4f11b563d3e8ed6c
Natha0b <laura.barona04@gmail.com>
2022-10-14 09:59:01 -0700

    task 3

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_isdigit - print the uppercase character
+ *@c: check caracter
+ *Return: zero and one
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= '0' && c <= '9')
+
+return (1);
+
+else
+
+return (0);
+}
