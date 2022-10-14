989add7e6a976530679c9ac331232cef92e56ea8
Natha0b <laura.barona04@gmail.com>
2022-10-14 14:59:17 -0700

    task 3

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *print_line - print line
+ */
+void print_line(int n)
+{
+
+int line;
+
+for (line = 1; line <= n; line++)
+{
+_putchar('_');
+}
+_putchar('\n')
+}
