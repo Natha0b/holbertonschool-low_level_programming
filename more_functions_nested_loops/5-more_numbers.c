98796893c8fecafb330f4442c7d735ae96ccd67c
Natha0b <laura.barona04@gmail.com>
2022-10-14 14:45:14 -0700

    task 3

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,20 +12,20 @@ void more_numbers(void)
 int numbers;
 int n;
 
-for (numbers = 0 ; numbers  9 ; numbers++)
+for (numbers = 0 ; numbers <= 9 ; numbers++)
 {
 for (n = 0 ; n <= 14 ; n++)
 {
 
 if (n > 9)
 {	
-_putchar((n / 10) + '0')
+_putchar((n / 10) + '0');
 }
 
 
-_putchar((n % 10) + '0')
+_putchar((n % 10) + '0');
 }
 
-_putchar('\n')
+_putchar('\n');
 }
 }
