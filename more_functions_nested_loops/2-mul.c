d594c24839518bdf5ae2c132f546a33dd311c01d
Natha0b <laura.barona04@gmail.com>
2022-10-14 10:12:20 -0700

    task 3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul - print the multiplies character
+ *@a: check caracter
+ *@b: check caracter
+ *Return: zero and one
+ */
+
+int mul(int a, int b)
+{
+
+printf("%d\n", mul(a, b))
+
+return (0);
+}
