392ad799b2bda801aa0f2b5eb8ae7f7477ee9a99
Natha0b <laura.barona04@gmail.com>
2022-10-14 12:22:39 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - print numbers
+ *numbers - variable
+ */
+void print_numbers(void);
+{
+int numbers
+
+for (numbers = '0' ; numbers <= '8' ; numbers++)
+
+{
+_putchar(numbers);
+}
+
+
+_putchar('\n');
+
+
+}
+
