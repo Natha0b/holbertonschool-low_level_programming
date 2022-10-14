c892648447a0834362d0942e77b3d963c70e0cce
Natha0b <laura.barona04@gmail.com>
2022-10-14 13:37:19 -0700

    task 3

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,14 @@
 void print_most_numbers(void)
 {
 
-int numbers;
+char numbers;
 
 for (numbers = '0' ; numbers <= '9' ; numbers++)
 {
 if (numbers != '2' && numbers != '4')
 
 {
-_putchar(numbers + '0');
+_putchar(numbers);
 }
 }
 _putchar('\n');
