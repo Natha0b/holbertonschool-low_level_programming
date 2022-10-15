5dea35c146a90c30fcd8589d7b151cbe5282fa26
Natha0b <laura.barona04@gmail.com>
2022-10-14 21:43:32 -0700

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *main - print triangle
+ *@size - size is size
+ */
+void print_triangle(int size)
+{
+    int colum;
+    int fil;
+    int x = size;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (fil = 0; fil < size; fil++)
+{
+x--;
+for (colum = 0; c < size; colum++)
+{
+if (colum < x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}     
