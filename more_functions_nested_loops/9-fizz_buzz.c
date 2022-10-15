8f4c27501467acaaef228674f4d4ea4735f5cec9
Natha0b <laura.barona04@gmail.com>
2022-10-14 20:51:11 -0700

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i < 101 ; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+if (i % 5 == 0)
+{
+printf("Buzz");
+}
+if (i % 3 != 0 && i % 5 != 0)
+{
+printf("%d", i);
+}
+if (i < 100) 
+{
+printf(" ");
+}
+
+}
+printf("\n");
+return (0);
+} 
