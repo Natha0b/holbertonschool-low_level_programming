44bcbec7467397d604770b0f25efcbb8b4db41b0
Natha0b <laura.barona04@gmail.com>
2022-10-14 19:18:03 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *print_square - print square
+ *@n: variable
+ */
+void print_square(int size)
+{
+int p;
+int s;
+
+if (size > 0)
+{
+while (p < size)
+{
+while (s < size)
+{
+_putchar(' ');
+s++;
+}
+_putchar('\n);
+p++;
+s = 0;
+}
+}
+else 
+_putchar('\n')
+}
+
