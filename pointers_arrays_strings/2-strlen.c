5ce33b3235cacba53a35ad9ddbf65aec41291647
Natha0b <laura.barona04@gmail.com>
2022-10-17 14:03:01 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - function a string
+ *@s: variable punter
+ *Return: zero
+ */
+
+int _strlen(char *s);
+{
+	char *s;
+	char cadena;
+	s = cadena;
+	s = &cadena[0];
+	return (0);
+}
