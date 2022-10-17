34ec33b5fa997c39164e134ecaac8af282118964
Natha0b <laura.barona04@gmail.com>
2022-10-17 11:07:11 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *swap_int - function a pointer
+ *@a: variable punter
+ *@b: variable punter
+ */
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
