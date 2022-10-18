4e78014fe979a69f6fc9ae136395bb09018018bb
Natha0b <laura.barona04@gmail.com>
2022-10-18 08:23:52 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,21 @@ void rev_string(char *s)
 {
 int r;
 int c = 0;
+int j;
+char l;
 
 while (s[c] != '\0')
 {
-c++;
-	
+c++;	
 }
-for (r = c - 1; r >= 0; r--)
-{
-r = s[c];
-s[c] = s[r];
-s[r] = r;
 
+j = x -1;
+
+for (r = 0; r < c /2; r++)
+{
+l = s[r];
+s[r] = s[j];
+s[j] = l
+j--;
 }
 }
