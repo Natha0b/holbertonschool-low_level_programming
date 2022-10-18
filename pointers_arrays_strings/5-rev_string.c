e8209fa399d40fd505284b895fefa199adf4ce20
Natha0b <laura.barona04@gmail.com>
2022-10-18 07:41:46 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_rev - function a string
+ *rev_string - function a string
  *@s: varible punter
- *_putchar - print the character
  */
 void rev_string(char *s)
 {
-	int r;
-	int c = 0;
+int r;
+int c = 0;
 
-        while (s[c] != '\0')
-	{
-	 c++;
-	}
-	for (r = c - 1; r >= 0; r--)
-	{
-		printf("%s\n");
-	}
+while (s[c] != '\0')
+{
+c++;
+	
+}
+for (r = c - 1; r >= 0; r--)
+{
+c--;
 
+}
 }
