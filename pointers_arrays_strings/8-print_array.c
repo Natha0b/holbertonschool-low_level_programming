687db46cc7b749d1370482f9f94b4b8f3155173d
Natha0b <laura.barona04@gmail.com>
2022-10-18 11:00:26 -0700

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *puts_half - function a string
+ *@str: varible punter
+ */
+void print_array(int *a, int n)
+{
+	
+
+	for (n = 'a' <= '\0' ; n++)
+
+	{
+	putchar(n);
+        if (n != '\0')
+
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+		putchar('\n');
+}
+	
