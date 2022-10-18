f2991c1180857237f621ded0e3f2306a45166556
Natha0b <laura.barona04@gmail.com>
2022-10-18 13:52:48 -0700

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *puts_half - function a string
- *@str: varible punter
+ **_strcat - function a string
+ *@dest: varible punter
+ *@src: varible punter
+ *Return: pointer
  */
 char *_strcat(char *dest, char *src)
 {
-	char p[2];
-	
-	p[0] = dest;
-	p[1] = '\0';
-	printf("%s\n" p, dest);
+	char *p = dest
+	int c;
+	int n;
+
+	for (c = 0; dest[c]; c++)
+	{
+
+	}
+	for (n = 0; src[n]; n++, c++)
+	{
+		dest[c] = src[n];
+	}
+
+	dest[c] = '\0';
+	return (*p)
+
 }
