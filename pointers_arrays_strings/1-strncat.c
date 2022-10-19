bd4e1586004a3ec44f70ec9e741faf19042dc0d9
Natha0b <laura.barona04@gmail.com>
2022-10-19 11:55:10 -0700

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strncat - function a string
+ *@dest: varible punter
+ *@src: varible punter
+ *@n: variable punter
+ *Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+        int c;
+	int i;
+
+
+	for (c = 0; dest[c]; c++)
+
+	{
+
+	}
+        for (n = 0; src[i]; n++, c++)
+	{
+	if (i < n)
+	{
+		dest[c + i] = src[i];
+		i++;
+	}
+	dest[c + i] = '\0';
+	}
+
+        _putchar('\n');
+	return (p);
+}
