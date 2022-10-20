716c22bf5af99b716073b1d419242c590d096d99
Natha0b <laura.barona04@gmail.com>
2022-10-20 08:36:04 -0700

    task5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  **string_toupper - function a string
- *@*c: variable pointer
+ *@c: variable pointer
  *Return: variable pointer
  */
 char *string_toupper(char *c)
@@ -13,9 +13,9 @@ char *string_toupper(char *c)
 	while (c[i])
 	{
 
-		if (c[i] >= 'a' && c[i] <= 'z')
-		       c[i] -= M;
-		i++;
+	if (c[i] >= 'a' && c[i] <= 'z')
+        c[i] -= M;
+	i++;
 	}
 
 	return (c);
