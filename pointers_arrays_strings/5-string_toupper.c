bc42697457f5533b4c7d3cd8c3eb17a645899f84
Natha0b <laura.barona04@gmail.com>
2022-10-20 07:58:22 -0700

    task5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **string_toupper - function a string
+ *@ *: variable punter
+ *Return: zero
+ */
+char *string_toupper(char *)
+{
+	int i;
+	int uppercase;
+
+	for (i = 0; *[i] != '\0'; i++)
+	{
+		i++;
+	}
+
+	for (uppercase = 'A' ; uppercase <= 'Z' ; uppercase++)
+	{}
+	return (0);
+}
+	
