6102625ad9dd06837617556f8933c42df31462bf
Natha0b <laura.barona04@gmail.com>
2022-10-21 08:32:59 -0700

    task4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,16 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-				
+
 				return (&s[i]);
 		}
 	}
-	
+
 	return ('\0');
-}       
+}
