6e6e445372e830080ba2fdbbaa0bd4dc07b93499
Natha0b <laura.barona04@gmail.com>
2022-10-21 08:20:47 -0700

    task4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strspn - function a string
+ *@s: variable pointer
+ *@accept: variable pointer
+ *Return: variable pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+       int i;
+       int j;
+ 
+       for (i = 0; s[i]; i++)
+       {
+	      for (j = 0; accept[j]; j++)
+	      {
+		     if (s[i] == accept[j]
+		     
+	             return (*s[i])
+	      }
+       }
+       return ('\0')
+
+}       
