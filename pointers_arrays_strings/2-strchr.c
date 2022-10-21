9008fcca0e9543def831649e95dd81fd09b9b97b
Natha0b <laura.barona04@gmail.com>
2022-10-20 19:21:38 -0700

    task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **_memset - function a string
+ *@s: variable pointer
+ *@b: variable pointer
+ *@n: variable pointer
+ *Return: variable pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (s[i] != '\0')
+
+	s = strchr (buf, 't');
+
+	if (s != NULL)
+	{
+		return ('\0')
+	}
+
+	return (0);
+}
