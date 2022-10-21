d62a7836093c14d40013474c802cf7086de9331e
Natha0b <laura.barona04@gmail.com>
2022-10-21 11:56:55 -0700

    task6

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **_strpbrk - function a string
+ *@s: variable pointer
+ *@accept: variable pointer
+ *Return: variable pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **_strpbrk - function a string
+ *@s: variable pointer
+ *@accept: variable pointer
+ *Return: variable pointer
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		
+
+			_putchar(a[i] a[j]);
+		        _putchar('\n');
+	}
+}
