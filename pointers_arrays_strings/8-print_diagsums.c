b611c4ee64ace05d28465ced977781068bdd3abf
Natha0b <laura.barona04@gmail.com>
2022-10-21 15:40:59 -0700

    task8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_chessboard - function a string
+ *@a: variable pointer
+ *Return: variable pointer
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s = 0;
+	int b = 0;
+
+	for (i = 0; i < size; i++)
+		s += a[i * (size + 1)];
+	printf("%d, ", s);
+
+	i = size -1;
+
+	while (i < (size * size) - (size - 1))
+	{ 
+		b += a[i];
+		i += (size -1);
+
+	}
+	printf("%d\n", b);
+
+
+
+}
