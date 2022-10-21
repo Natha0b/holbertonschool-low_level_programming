c25b2a52040208c0f5ed04d9edccaf76e24385b1
Natha0b <laura.barona04@gmail.com>
2022-10-21 12:22:02 -0700

    task6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- **_strpbrk - function a string
- *@s: variable pointer
- *@accept: variable pointer
+ *print_chessboard - function a string
+ *@a: variable pointer
  *Return: variable pointer
  */
 void print_chessboard(char (*a)[8])
@@ -13,10 +12,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-        for (j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 
 
 	_putchar(a[i][j]);
-        _putchar('\n');
+		_putchar('\n');
 	}
 }
