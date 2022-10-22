cb8d585798ef47c97d06892615b891524ab2255b
Natha0b <laura.barona04@gmail.com>
2022-10-21 17:50:10 -0700

    task5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,33 +9,34 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-        int x;
+	int x;
 	int aux;
 	int aux1;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		aux = i;
-		
+
 		for (x = 0; needle[x] != '\0'; x++, aux++)
 		{
 			if (haystack[aux] != needle[x])
 			{
 				aux1 = 0;
-				
+
 				break;
 			}
 		else
 		{
 			aux1++;
 		}
-		}						
-		
+		}
+
 		if (aux1 != 0)
 		{
 			return (haystack + i);
 		}
 	}
-        return ('\0');
+
+	return ('\0');
 }
 
