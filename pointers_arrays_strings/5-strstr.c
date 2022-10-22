c7b64698c5a185ab4a740786d5ccd56a0422120a
Natha0b <laura.barona04@gmail.com>
2022-10-21 17:24:48 -0700

    task5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 /**
- **_strpbrk - function a string
- *@s: variable pointer
- *@accept: variable pointer
+ **_strstr - function a string
+ *@haystack: variable pointer
+ *@needle: variable pointer
  *Return: variable pointer
  */
 char *_strstr(char *haystack, char *needle)
 {
 
+	int i;
+	int j;
+	int c;
+
+	for (j = 0; needle[j] != '\0')
+
+		c = j;
+
+	while (haystack[i + c - 1] != '\0')
+	{
+		j = 0;
+
+		while (haystack[i + j] == needle[j] && needle[j])
+		{
+			j++;
+		}
+
+		if (j == c)
+			return (haystack[i]);
+		i++;
+	}
+	return ('\0');
+}
