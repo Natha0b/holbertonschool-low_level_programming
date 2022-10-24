2cd4e5a370df81769d75108e5e2f2ce88b2e3d8e
Natha0b <laura.barona04@gmail.com>
2022-10-24 13:50:40 -0700

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *factorial - function a recursion
+ *@n: varible recursion
+ *Return: variable recursion
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (- 1)
+	return _pow_recursion(x, y)
+}
+
