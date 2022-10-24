c80680fcbff4cb961c4c35397b9ae6c24764ec54
Natha0b <laura.barona04@gmail.com>
2022-10-24 10:33:26 -0700

    task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen_recursion - function a recursion
+ *@s: varible recursion
+ */
+int _strlen_recursion(char *s)
+{
+	int c = 0;
+
+	if (s[c] == '\0')
+        {
+		_strlen_recursion(s + 1);
+		
+		_putchar(s[c]);
+
+	}
+}
