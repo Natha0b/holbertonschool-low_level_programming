8bde8b0c2db27c72898ce6c7d0c23e69db30687d
Natha0b <laura.barona04@gmail.com>
2022-10-24 08:56:10 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,11 +9,11 @@ void _print_rev_recursion(char *s)
 	int i;
 	int c = 0;
 
-	while (s[c] != '\0')
+	if (s[c] != '\0')
 	{
 		c++;
 	}
-	for (i = c - 1; i >= 0; i--)
+	else if (i = c - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
