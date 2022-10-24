dce7504c253c7d7e1aed85db2921856165ebde44
Natha0b <laura.barona04@gmail.com>
2022-10-24 16:11:21 -0700

    task5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_sqrt_recursion - function a recursion
+ *@n: variable recursion
+ *Return: variable recursion
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (n * _sqrt_recursion(n - 1));
+
+}
