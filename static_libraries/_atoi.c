41378afeb30f091da9e0f0fb503f957823b4d864
Natha0b <laura.barona04@gmail.com>
2022-10-25 19:00:22 -0700

    task0

--- static_libraries/_atoi.c
+++ static_libraries/_atoi.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- **_strcpy - function a string
- *@dest: varible punter
- *@src: varible punter
+ *_atoi - function a string
+ *@s: varible punter
  *Return: pointer
  */
 int _atoi(char *s)
