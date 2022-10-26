cd229a7deabf32ae85dbcf6fbcba3d913d71d397
Natha0b <laura.barona04@gmail.com>
2022-10-26 09:51:42 -0700

    task0

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main- function argc argv
+ *@argc: varible argc, argv
+ *@argv: variable argc, argv
+ *@n: variable
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+		printf("%s\n", argv[n]);
+
+	return (0);
+}
