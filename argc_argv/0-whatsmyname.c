6df7d64b3ba065f1ef62b819fc4b8bcaf732a9e2
Natha0b <laura.barona04@gmail.com>
2022-10-26 09:24:15 -0700

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  *_puts_recursion - function a recursion
  *@s: varible recursion
  */
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 		return (0);
