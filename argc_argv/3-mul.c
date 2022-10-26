96b8380e801833a0f83af736af44cc04d4de58ac
Natha0b <laura.barona04@gmail.com>
2022-10-26 11:03:39 -0700

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main- function argc argv
+ *@argc: varible argc, argv
+ *@argv: variable argc, argv
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int dig1 = 0;
+	int dig2 = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		dig1 = atoi(argv[1]);
+		dig2 = atoi(argv[2]);
+
+		printf("%d\n", dig1 * dig2);
+	}
+	return (0);
+}
+
+
