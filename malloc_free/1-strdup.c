d38eb69e7d40374f37995bb4d7b663c0084f9931
Natha0b <laura.barona04@gmail.com>
2022-10-31 12:15:04 -0500

    task0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ **_strdup- function malloc and free
+ *@str: variable malloc
+ *Return: zero
+ */
+char *_strdup(char *str);
+{
+    int i;
+
+    void free(void *str);
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    return (str);
+}
