94a5609458d3067462feb27894209c42c9684225
Natha0b <laura.barona04@gmail.com>
2022-10-31 14:30:04 -0500

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,13 +16,11 @@ char *_strdup(char *str)
     {
         return (NULL);
     }
-
     for (i = 0; str[i] != '\0'; i++)
     {
     }
-
     a = malloc((i + 1) * sizeof(char));
-
+    
     if (a == NULL)
     {
         return (NULL);
@@ -31,8 +29,7 @@ char *_strdup(char *str)
     {
         a[s] = str[s];
     }
-
     a[s] = '\0';
-
+    
     return (a);
 }
