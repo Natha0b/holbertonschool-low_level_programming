868b685faa3701a4dc334577df25e4589563d8e8
Natha0b <laura.barona04@gmail.com>
2022-10-31 18:25:29 -0500

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,9 +9,45 @@
  */
 int **alloc_grid(int width, int height)
 {
-	char *a;
+	int **a;
+	int i;
+	int n;
+	int l;
 
+	if (width < 0)
+	{
+		return (NULL);
+	}
+	if (height < 0)
+	{
+		return (NULL);
+	}
 
+	a = malloc(height * sizeof(int *));
 
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(width * sizeof(int *));
 
+		if (a == NULL)
+		{
+			return (NULL);
+		}
+
+		for (; i > 0; i--)
+		{
+			free(a[i]);
+		}
+
+	}
+	for (n = 0; n < height; n++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			a[n][l] = 0;
+		}
+
+	}
+
+	return (a);
 }
