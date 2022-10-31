0de4375984b0a438639fac34488223113c17dfa4
Natha0b <laura.barona04@gmail.com>
2022-10-31 07:53:55 -0700

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdli.h>
+#include "main.h"
+/**
+ *main- function argc argv
+ *@argc: varible argc, argv
+ *@argv: variable argc, argv
+ *Return: zero
+ */
+char *create_array(unsigned int size, char c)
+{
+	for (c = 0; c < 4; c++)
+		 A[c] = size;
+
+return (0);	
+}
