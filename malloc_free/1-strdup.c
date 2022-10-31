63962703f3f1f304cad552524052584830725914
Natha0b <laura.barona04@gmail.com>
2022-10-31 14:32:27 -0500

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,28 +8,28 @@
  */
 char *_strdup(char *str)
 {
-    char *a;
-    int i;
-    int s;
+char *a;
+int i;
+int s;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; str[i] != '\0'; i++)
-    {
-    }
-    a = malloc((i + 1) * sizeof(char));
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+a = malloc((i + 1) * sizeof(char));
+ 
+if (a == NULL)
+{
+return (NULL);
+}
+for (s = 0; str[s] != '\0'; s++)
+{
+a[s] = str[s];
+}
+a[s] = '\0';
     
-    if (a == NULL)
-    {
-        return (NULL);
-    }
-    for (s = 0; str[s] != '\0'; s++)
-    {
-        a[s] = str[s];
-    }
-    a[s] = '\0';
-    
-    return (a);
+return (a);
 }
