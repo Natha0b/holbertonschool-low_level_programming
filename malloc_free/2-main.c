d353e6f981acf429ac0c793761523bcf7b17401e
Natha0b <laura.barona04@gmail.com>
2022-10-31 16:44:02 -0500

    task2

--- malloc_free/2-main.c
+++ malloc_free/2-main.c
@@ -1,23 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+char *str_concat(char *, char *);
+
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = str_concat("Best ", "School");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = str_concat("Hello", "");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,10 +15,6 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	char *a;
 
-	if (s1 == NULL)
-	{
-		return (NULL);
-	}
 
 	for (c = 0; s1[c] != '\0'; c++)
 	{
@@ -26,18 +22,33 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (n = 0; s2[n] != '\0'; n++)
+	{
+
+	}
 
 	a = malloc((c + n) * sizeof(char));
 
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	if (c > 0)
+	{
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		a[i] = s1[i];
 	}
+	}
+
+	if (n > 0)
+	{
 
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		a[i] = s2[j];
 		i++;
+	}
 	}
 
 	return (a);
