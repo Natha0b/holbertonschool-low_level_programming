40483980d2f46efc6d65f4a046039089d8a904cd
Natha0b <laura.barona04@gmail.com>
2022-11-01 17:19:51 -0500

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,20 +32,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		a = malloc((c + l + 1) * sizeof(char));
 	}
-	else 
+	else
 	{
 		a = malloc((c + n + 1) * sizeof(char));
 	}
 	if (a == NULL)
 		return (NULL);
-
 	for (i = 0; i < c; i++)
 		a[i] = s1[i];
-
 	for (j = 0; j < n && j < l; j++, i++)
 		a[i] = s2[j];
 
 	a[i] = '\0';
-
 	return (a);
 }
