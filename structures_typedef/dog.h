861a28e392acb11665a98dde6cf14e359e31c263
Natha0b <laura.barona04@gmail.com>
2022-11-08 09:59:22 -0500

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
-typedef struct dog
+#ifndef DOG
+#define DOG
+/**
+ *struct dog- function a Structures, typedef
+ *@name: variable typedef
+ *@owner: variable typedef
+ *@age: variable type def
+ */
+struct dog
 {
 	char *name, *owner;
 	float age;
 
-}dog;
+} dog;
+#endif
