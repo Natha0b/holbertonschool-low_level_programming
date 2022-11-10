476e99b5d635ae7606937be496c3b9bd437fa8d7
Natha0b <laura.barona04@gmail.com>
2022-11-10 17:25:49 -0500

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		pointer =  va_arg(ptr, char *);
 
 		if (pointer == NULL)
-			printf("nil");
+			printf("(nil)");
 
 		else
 			printf("%s", pointer);
