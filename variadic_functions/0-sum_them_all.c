bea29896ba8fda45b22224f4240d4c6ad81af5e1
Natha0b <laura.barona04@gmail.com>
2022-11-10 10:15:30 -0500

    task0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,12 +1,28 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 /**
  * sum_them_all- sum  of all its parameters
  * @const: variable const
  * @n: variable sum
-*/
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	
-}
+	int sum, i;
+	va_list ptr;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	sum = 0;
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+
+
+	va_end(ptr);
+	return (sum);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef VARIA_H
+#define VARIA_H
+#include <stdarg.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
