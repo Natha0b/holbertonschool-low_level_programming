21b1ed1921a27722e3e2f9d244dc97889ca7c63d
Natha0b <laura.barona04@gmail.com>
2022-11-10 09:39:03 -0500

    r

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all- sum  of all its parameters
+ * @const: variable const
+ * @n: variable sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	
+}
+
