98a90f2639bd3f0febc7cfef67910fcb759e8cad
Natha0b <laura.barona04@gmail.com>
2022-11-10 19:17:27 -0500

    task3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all- sum  of all its parameters
+ * @format: variable char
+ *
+ */
+void print_all(const char *const format, ...)
+{
+	va_list ptr;
+	unsigned int count;
+	char *p, c;
+	int i;
+	float f;
+
+	count = 0;
+	va_start(ptr, format);
+	while (format != NULL && format[count++])
+	{
+		switch (format[count++])
+		{
+		case 'c':
+			c = (char)va_arg(ptr, int);
+			printf("%c", c);
+			break;
+		case 'i':
+			i = va_arg(ptr, int);
+			printf("%d", i);
+			break;
+		case 'f':
+			f = (float)va_arg(ptr, double);
+			printf("%f", f);
+			break;
+		case 'p':
+			p = va_arg(ptr, char *);
+			if (p == NULL)
+				p = "(nil)";
+
+			printf("%s", p);
+			break;
+		default:
+			continue;
+		}
+		if (format[count] != '\0')
+			printf(", ");
+	}
+	va_end(ptr);
+	printf("\n");
+}
