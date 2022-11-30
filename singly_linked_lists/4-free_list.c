31fa8caf8f18867ac74f4943272f6decaccc100d
Natha0b <laura.barona04@gmail.com>
2022-11-29 19:30:18 -0500

    free memory

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list- function
+ *@head: variable
+ */
+
+void free_list(list_t *head)
+{
+
+	while (head == NULL)
+		return;
+
+	free(head->next);
+	free(head->str);
+	free(head);
+}
