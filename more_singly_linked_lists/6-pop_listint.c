1cbd644c3dfa6b0008763227f7f41d2a060c3a2b
Natha0b <laura.barona04@gmail.com>
2022-11-30 19:21:52 -0500

    get node int at index

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *pop_listint- function
+ *@head: variable
+ */
+
+int pop_listint(listint_t **head)
+{
+
+}
---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ **get_nodeint_at_index- function
+ *@head: variable
+ *@index: variable
+ *Return: Variable head
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int j;
+
+	for (j = 0; j < index; j++)
+		head = head->next;
+
+	if (head == NULL)
+		return (NULL);
+
+	return (head);
+}
