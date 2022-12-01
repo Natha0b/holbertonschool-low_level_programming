fedd3acf8b02cc4250fa9eeeeb304e00e9557bbc
Natha0b <laura.barona04@gmail.com>
2022-12-01 17:52:41 -0500

    pop list int

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -3,9 +3,22 @@
 /**
  *pop_listint- function
  *@head: variable
+ *Return: variable n
  */
 
 int pop_listint(listint_t **head)
 {
+	int n;
+	listint_t *new;
 
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+
+	new = (*head)->next;
+	free(*head);
+	*head = new;
+
+	return (n);
 }
