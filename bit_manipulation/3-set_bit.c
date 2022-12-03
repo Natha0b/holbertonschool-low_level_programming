bbe0c2b18fe9752799eeaf5c21b5ce62d2f1df4e
Natha0b <laura.barona04@gmail.com>
2022-12-02 19:07:30 -0500

    set bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -3,14 +3,14 @@
 /**
  *set_bit- function
  *@n: variable
- *@index: variable 
+ *@index: variable
  *Return: variable
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
-	return (-1);
+		return (-1);
 
 	*n = *n | (1 << index);
 
