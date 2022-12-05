0520a51256c7e39521631c000dd4bfec83cff03b
Natha0b <laura.barona04@gmail.com>
2022-12-05 16:27:55 -0500

    changes in add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,23 +10,25 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
+	new_node->prev = NULL;
 
-	if (!head)
-		head = &new_node;
-	if (!(*head))
-		*head = new_node;
+	if (head)
+		new_node->next = *head;
+
+	if (*head)
+		(*head)->prev = new_node;
 
 	else
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = NULL;
 	}
+	*head = new_node;
+
 	return (new_node);
 }
