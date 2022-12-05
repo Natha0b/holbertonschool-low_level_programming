a12d61f2986bc0ca7ec0261607b846cb249c6411
Natha0b <laura.barona04@gmail.com>
2022-12-05 16:07:05 -0500

    d list int len

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *dlistint_len- function
+ *@h: variable
+ *Return: variable
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
