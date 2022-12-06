f29ad460b85e9f16be40f5e37646d4d9c6adadf1
Natha0b <laura.barona04@gmail.com>
2022-12-06 10:24:53 -0500

    changes in insert double node int

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,37 +1,5 @@
 #include "lists.h"
 
-/**
- **add_dnodeint- function
- *@head: variable
- *@n: variable
- *Return: variable
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new_node;
-
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-
-	if (head)
-		new_node->next = *head;
-
-	if (*head)
-		(*head)->prev = new_node;
-
-	else
-	{
-		new_node->next = NULL;
-	}
-	*head = new_node;
-
-	return (new_node);
-}
 
 /**
  **add_dnodeint- function
