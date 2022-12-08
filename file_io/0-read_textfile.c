1be49f63b69cbd9540f73c512c2f923756cb93a4
Natha0b <laura.barona04@gmail.com>
2022-12-08 11:38:39 -0500

    read text file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,16 +7,34 @@
  *Return: variable
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char filename, size_t letters)
 {
-	ssize_t fd;
+	char *buff;
+	int fd, read_f, write_f;
 
-	fd = open("filename", O_CREAT | O_WRONLY, 0600);
-
-	if (filename == NULL)
+	if (!filename)
+		return (0);
+		
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	
+	buff = malloc(sizeof(char) * letters + 1);
+	if (!buff)
+		return (0);
 
-	
+	read_f = read(fd, buff, letters);
+	if (read_f == -1)
+		return (0);
+
+	buff[letters] = '\0';
+
+	write_f = write(STDOUT_FILENO, buff, read_f);
+
+	if (write_f == -1)
+		return (0);
+
+	free(buff);
+	close(fd);
+	return (write_f);
 }
