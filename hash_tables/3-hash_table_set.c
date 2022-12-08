4984be4c0986bdd11844f2952fc69526c1471864
Natha0b <laura.barona04@gmail.com>
2022-12-08 17:20:05 -0500

    hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,13 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * key_index -  function.
- * @key: const unsigned char variable
- * @size: const unsigned variable
+ * hash_table_set- function that adds an element to the hash table.
+ * @key: const char
+ * @ht: hash_table_t
+ * @value: const char
  * Return: unsigned long integer
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	unsigned long int index;
+	hash_node_t *node = NULL;
+	char *value_dup, *key_dup;
+
+	value_dup = strdup(value);
+	key_dup = strdup(key);
+
+	index = hash_djb2((const unsigned char *)key_dup) % (ht->size);
+
+	node = malloc(sizeof(hash_node_t));
+
+	node->key = key_dup;
+	node->value = value_dup;
+	node->next = NULL;
+
+	if(ht->array[index])
+		node->next = ht->array[index];
+
+	ht->array[index] = node;
+
+	return (1);
 }
