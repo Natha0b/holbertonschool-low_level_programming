f428d9a31a2b4072db85c42058318d230e32ec95
Natha0b <laura.barona04@gmail.com>
2022-12-09 09:34:35 -0500

    changes in function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	value_dup = NULL;
 	node->next = NULL;
 
-	if (ht->array[index])
+	if (ht->array[index] && strcmp(ht->array[index]->key, key_dup) != 0)
 		node->next = ht->array[index];
 
 	ht->array[index] = node;
