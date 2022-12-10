d779117791ec580ca862b1448c5dc3eb679a2569
Natha0b <laura.barona04@gmail.com>
2022-12-09 19:10:59 -0500

    changes in files

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] && strcmp(ht->array[index]->key, key_dup) != 0)
 		node->next = ht->array[index];
-	
+
 	else if (ht->array[index])
 	{
 		free(ht->array[index]->key);
