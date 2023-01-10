82f1a71925e0e21d503a1dbea59efd0d3ab413b7
Natha0b <laura.barona04@gmail.com>
2023-01-10 17:12:14 -0500

    create files

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ *linear_search- function that searches for a value
+ *@array: variable integrer
+ *@size: variable
+ *@value: variable integrer
+ *Return: zero
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || !value)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+ * binary_search - function that searches for a value
+ *@array: variable integrer
+ *@size: variable
+ *@value: varaible integrer
+ *Return: zero
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0, mid = 0, high = size - 1, i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+	while (low <= high)
+	{
+		mid = (high + low) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			if (i != low)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
